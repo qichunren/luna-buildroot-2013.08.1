fa92c52bde5d35c4da7e87f0fa594824aadb182a
yiling.cao@gmail.com <Yiling Cao>
2013-11-11 16:02:06 +0800

    add flashing led and add autoupgrader script.

--- overlay/root/autoupgrade.sh
+++ overlay/root/autoupgrade.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-echo -n "Upgrading in 3 seconds, press any key to cancel!"
+echo  " " #empty line
+echo  " " #empty line
+echo  "=================LUNA UPGRADER==================" #title line
+echo  "Upgrading in 3 seconds, press any key to cancel!"
 
-if read -t 3 response; then
+/root/flash_led.sh &
+
+if read -t 4 response; then
 	echo "Cancelled!"
 	/bin/login -f root
 else
---
+++ overlay/root/flash_led.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#flash led during upgrading
+mount -t debugfs none /sys/kernel/debug
+
+
+
+echo 7 >  /sys/kernel/debug/omap_mux/usb0_drvvbus
+echo 7 >  /sys/kernel/debug/omap_mux/mcasp0_ahclkx
+
+echo 117 > /sys/class/gpio/export
+echo 18  > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio117/direction
+echo out > /sys/class/gpio/gpio18/direction
+
+while true; 
+do
+	echo 1 > /sys/class/gpio/gpio117/value
+	echo 0 > /sys/class/gpio/gpio18/value
+
+	sleep 2
+	echo 1 > /sys/class/gpio/gpio18/value
+	usleep 10000
+	echo 0 > /sys/class/gpio/gpio18/value 
+	echo 0 > /sys/class/gpio/gpio117/value
+	usleep 50000
+	echo 1 > /sys/class/gpio/gpio117/value; usleep 40000
+	echo 0 > /sys/class/gpio/gpio117/value; usleep 40000
+	echo 1 > /sys/class/gpio/gpio117/value; usleep 40000
+	echo 0 > /sys/class/gpio/gpio117/value; usleep 40000
+	echo 1 > /sys/class/gpio/gpio117/value; usleep 40000
+	echo 0 > /sys/class/gpio/gpio117/value; usleep 40000
+done

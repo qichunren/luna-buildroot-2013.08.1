8428d18bb399455d1f1a2700b3c45d49189d1dc6
Kyosuke Nekomura <supercatexpert@gmail.com>
2016-09-23 22:41:38 +0900

    Be able to update modules & kernel.

--- overlay/root/update_rootfs.sh
+++ overlay/root/update_rootfs.sh
@@ -4,20 +4,23 @@ set -xe
 
 DIR=`pwd`
 MMC_DEV=/dev/mmcblk0
+BOOTFS_PART=${MMC_DEV}p1
 ROOTFS_PART=${MMC_DEV}p2
 VARDIR_PART=${MMC_DEV}p4
 
+BOOTFS_PATH=/tmp/bootfs
 ROOTFS_PATH=/tmp/rootfs
 VARDIR_PATH=/tmp/vardir
 
 
 mount_parts(){
   ## make dir if missing
+  mkdir -p ${BOOTFS_PATH}
   mkdir -p ${ROOTFS_PATH}
   mkdir -p ${VARDIR_PATH}
 
   ## mount to correct dir
-  mount ${ROOTFS_PART} ${ROOTFS_PATH}
+  mount ${BOOTFS_PART} ${BOOTFS_PATH}
   mount ${VARDIR_PART} ${VARDIR_PATH} #we don't need update var!
 }
 
@@ -29,19 +32,30 @@ update_parts(){
     echo "No update packages, skipping."
   fi
 
-  #flash parts 
-  cd ${ROOTFS_PATH}
-  rm -rf *
-  xzcat ${VARDIR_PATH}/dl/rootfs.tar.xz | tar xv
+  #flash parts
+  mkfs.ext4 ${ROOTFS_PART}
+  mount ${ROOTFS_PART} ${ROOTFS_PATH}
 
+  cd ${ROOTFS_PATH}
+  xzcat ${VARDIR_PATH}/dl/rootfs.tar.xz | tar xv
+  
+  if [ -f ${VARDIR_PATH}/dl/kmods.tar.xz ]; then
+    xzcat ${VARDIR_PATH}/dl/kmods.tar.xz | tar xv
+  fi
+ 
   #we don't need update var!
   cd ${ROOTFS_PATH}/var 
   rm -rf *
 
+  if [ -f ${VARDIR_PATH}/dl/uImage ]; then
+    cp ${VARDIR_PATH}/dl/uImage ${BOOTFS_PATH}/uImage
+  fi
 }
 
 cleaning(){
   rm ${VARDIR_PATH}/dl/rootfs.tar.xz || true
+  rm ${VARDIR_PATH}/dl/kmods.tar.xz || true
+  rm ${VARDIR_PATH}/dl/uImage || true
 }
 
 verify(){
@@ -51,6 +65,7 @@ verify(){
 
 umount_parts(){
   cd ${DIR}
+  umount ${BOOTFS_PART} || true
   umount ${ROOTFS_PART} || true
   umount ${VARDIR_PART} || true #we don't need update var!
 }

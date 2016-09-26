302c626a90f5d4337f6b9bb47c98abe57c5dd99f
Kyosuke Nekomura <supercatexpert@gmail.com>
2016-09-26 19:06:47 +0900

    Enable GPIO & Set audio channels for upgrade notify.

--- overlay/root/update_rootfs.sh
+++ overlay/root/update_rootfs.sh
@@ -12,6 +12,40 @@ BOOTFS_PATH=/tmp/bootfs
 ROOTFS_PATH=/tmp/rootfs
 VARDIR_PATH=/tmp/vardir
 
+if [ ! -f "/tmp/paigo-gpio.tmp" ]; then
+    echo "56" >/sys/class/gpio/export
+    echo "out" >/sys/class/gpio/gpio56/direction
+    echo "1" >/sys/class/gpio/gpio56/value
+
+    echo "65" >/sys/class/gpio/export
+    echo "out" >/sys/class/gpio/gpio65/direction 
+    echo "1" >/sys/class/gpio/gpio65/value
+
+    echo "48" >/sys/class/gpio/export
+    echo "out" >/sys/class/gpio/gpio48/direction
+    echo "1" >/sys/class/gpio/gpio48/value
+
+
+    echo "55" > /sys/class/gpio/export
+    echo "in" >/sys/class/gpio/gpio55/direction 
+
+    echo "49" > /sys/class/gpio/export
+    echo "in" >/sys/class/gpio/gpio49/direction 
+
+    echo "50" > /sys/class/gpio/export
+    echo "out" >/sys/class/gpio/gpio50/direction 
+
+    echo "51" > /sys/class/gpio/export
+    echo "out" >/sys/class/gpio/gpio51/direction
+
+    touch "/tmp/paigo-gpio.tmp"
+fi
+
+/usr/bin/amixer sset 'Left Line Mixer Line2L Bypass' off || true
+/usr/bin/amixer sset 'Left Line Mixer Line2R Bypass' mute || true
+/usr/bin/amixer sset 'PCM' 100% || true
+/usr/bin/amixer sset 'Line DAC' 100% || true
+/usr/bin/amixer sset 'Line' on || true
 
 mount_parts(){
   ## make dir if missing
